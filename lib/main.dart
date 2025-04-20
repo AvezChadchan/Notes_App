dc602fe1bfa5f14a084c30026e48f13f348f7d75
Avez Chadchan <avezchadchan@gmail.com>
2025-04-20 22:16:34 +0530

    Day 1: Project setup and basic UI layout

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Notes App',
+      theme: ThemeData(),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Notes App"),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+      body:
+        ListView(
+          children: [],
+        ),
+      floatingActionButton: FloatingActionButton(onPressed: () {  },child: Icon(Icons.add),),
+    );
+  }
+}
